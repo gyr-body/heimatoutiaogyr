08d73bb79ced725e5c9db722f43bff6658234f89
gyr <guoyongyong-6@123.com>
2019-12-18 22:03:15 +0800

    改错

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './styles/index.less' // 引用默认样式
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
-
+Vue.use(ElementUI) // 全局注册
 new Vue({
   router,
   render: h => h(App)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,49 @@
 <template>
-<div>登录页面</div>
+  <div class="login">
+    <div class="login-card">
+      <div class="title">
+        <img src="../../assets/img/logo_index.png" alt srcset />
+      </div>
+      <el-form ref="">
+          <el-form-item>
+              <el-input placeholder="请您输入手机号"></el-input>
+          </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data: function () {
+    return {
+      model: ' ',
+      code: ' ',
+      check: ' '
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less">
+.login {
+  background-image: url("../../assets/img/login_bg.jpg");
+  background-size: cover; // 自适应背景图片
+  height: 100vh; //当前可视区域的百分之百的高度
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-card {
+    width: 450px;
+    height: 300px;
+    background-color: white;
+    .title {
+        margin-top: 10px;
+      text-align: center;
+      img {
+        height: 40px;
+      }
+    }
+  }
+}
 </style>

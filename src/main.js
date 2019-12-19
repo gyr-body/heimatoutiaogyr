cdb3de05cbc21d5edc0874b94d73c24835deecf2
gyr <guoyongyong-6@123.com>
2019-12-19 15:20:32 +0800

    配置全局使用  补充

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import './styles/index.less'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import axios from 'axios'
+Vue.prototype.$axios = axios
+Vue.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 全局注册
 new Vue({

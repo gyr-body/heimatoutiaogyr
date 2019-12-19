f5688cb151dc74085ddbef73838d8e011a934204
gyr <guoyongyong-6@123.com>
2019-12-19 19:09:48 +0800

    主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>我是主页</div>
+  <!-- 外层容器 -->
+  <el-container>
+    <!-- 侧边栏容器 -->
+    <el-aside>左侧</el-aside>
+    <!-- 上下布局 -->
+    <el-container>
+      <!-- 顶栏容器 -->
+      <el-header>顶部</el-header>
+      <!-- 主要区域容器 -->
+      <el-main>主要区域</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>

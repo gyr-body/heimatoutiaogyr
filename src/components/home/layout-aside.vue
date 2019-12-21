de000759289d4dea51da9c9b0585cc954b6de29b
gyr <guoyongyong-6@123.com>
2019-12-21 16:07:03 +0800

    主页模块-左侧导航配置路由

--- src/components/home/layout-aside.vue
+++ src/components/home/layout-aside.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="layout-aside">
-      <div class="title">
-          <img src="../../assets/img/logo_admin.png" alt srcset />
-      </div>
-      <el-menu background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b">
-          <el-menu-item>首页</el-menu-item>
-          <el-submenu index="1">
-              <span slot="title">内容管理</span>
-              <el-menu-item>发布文章</el-menu-item>
-              <el-menu-item>内容列表</el-menu-item>
-              <el-menu-item>评论列表</el-menu-item>
-              <el-menu-item>素材管理</el-menu-item>
-          </el-submenu>
-          <el-submenu index="2">
-              <span slot="title">粉丝管理</span>
-              <el-menu-item>图文数据</el-menu-item>
-              <el-menu-item>粉丝画像</el-menu-item>
-              <el-menu-item>粉丝概况</el-menu-item>
-              <el-menu-item>粉丝列表</el-menu-item>
-          </el-submenu>
-          <el-menu-item>账户信息</el-menu-item>
-      </el-menu>
+    <div class="title">
+      <img src="../../assets/img/logo_admin.png" alt srcset />
+    </div>
+    <el-menu router background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b" >
+      <el-menu-item index="/home">首页</el-menu-item>
+      <el-submenu index="1">
+        <span slot="title">内容管理</span>
+        <el-menu-item index="/home/publish">发布文章</el-menu-item>
+        <el-menu-item index="/home/articles">内容列表</el-menu-item>
+        <el-menu-item index="/home/comment">评论列表</el-menu-item>
+        <el-menu-item index="/home/material">素材管理</el-menu-item>
+      </el-submenu>
+      <el-submenu index="2">
+        <span slot="title">粉丝管理</span>
+        <el-menu-item index="/home/picture">图文数据</el-menu-item>
+        <el-menu-item index="/home/fansinfo">粉丝画像</el-menu-item>
+        <el-menu-item index="/home/fanslife">粉丝概况</el-menu-item>
+        <el-menu-item index="/home/fanslist">粉丝列表</el-menu-item>
+      </el-submenu>
+      <el-menu-item>账户信息</el-menu-item>
+    </el-menu>
   </div>
 </template>
 
@@ -30,16 +30,15 @@ export default {}
 
 <style lang="less" scoped>
 .layout-aside {
-    // width: 230px;
-    overflow: hidden;
-    .title {
-        text-align: center;
-        background-color: #2e2f32;
-        padding: 10px 0;
-        img {
-            height: 35px;
-        }
-
+  // width: 230px;
+  overflow: hidden;
+  .title {
+    text-align: center;
+    background-color: #2e2f32;
+    padding: 10px 0;
+    img {
+      height: 35px;
     }
+  }
 }
 </style>

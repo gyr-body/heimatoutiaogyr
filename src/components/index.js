80bb71a0eeddc91c05bf5ffa0c16de7589289a08
gyr <guoyongyong-6@123.com>
2019-12-22 02:03:39 +0800

    面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,10 @@
 import LayoutAside from './home/layout-aside'
 import LayoutHeader from './home/layout-header'
-
+import BereadCrumb from '../components/bread-crumb'
 export default {
   install (Vue) {
     Vue.component('layout-aside', LayoutAside) // 全局注册
     Vue.component('layout-header', LayoutHeader) // 全局注册
+    Vue.component('bread-crumb', BereadCrumb) // 面包屑组件
   }
 }

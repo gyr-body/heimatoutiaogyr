f2394c3ca16f8079b3e3b0b621601d3e27b1e0b8
gyr <guoyongyong-6@123.com>
2019-12-22 19:45:42 +0800

    评论列表-评论数据加载到页面及状态过滤

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,27 +1,57 @@
 <template>
-<!-- 卡片组件 -->
+  <!-- 卡片组件 -->
   <el-card>
-      <!-- 面包屑组件 -->
-      <bread-crumb>
+    <!-- 面包屑组件 -->
+    <bread-crumb>
       <!-- 具名插槽 -->
-        <template slot="title">评论列表</template>
-      </bread-crumb>
-      <el-table>
-          <el-table-column label="标题"></el-table-column>
-          <el-table-column label="评论状态"></el-table-column>
-          <el-table-column label="总评论数"></el-table-column>
-          <el-table-column label="粉丝评论数"></el-table-column>
-          <el-table-column label="操作"></el-table-column>
-      </el-table>
+      <template slot="title">评论列表</template>
+    </bread-crumb>
+    <el-table :data="list">
+      <el-table-column prop="title" label="标题" width="500"></el-table-column>
+      <el-table-column :formatter="formatterBool" prop="comment_status" label="评论状态"></el-table-column>
+      <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+      <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="obj">
+          <el-button type="text">修改</el-button>
+          <el-button type="text">{{obj.row.comment_status ? "关闭评论" : "打开评论" }}</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    getComment () {
+      // 请求列表数据
+      this.$axios({
+        url: '/articles',
+        params: { response_type: 'comment' }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    },
+    formatterBool (row, column, cellValue, index) {
+      // row  当前行数据
+      // column 当前列属性
+      // cellValue 当前单元格的值
+      // index  当前下标
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    // 调用方法
+    this.getComment()
+  }
 }
 </script>
 
 <style>
-
 </style>

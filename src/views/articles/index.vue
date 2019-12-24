856ab691fcae5cceed894a8473664b46b6eb409d
gyr <guoyongyong-6@123.com>
2019-12-24 21:28:25 +0800

    内容列表-页面主体结构

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -10,7 +10,7 @@
       <el-col :span="18">
         <!-- 单选框 -->
         <el-radio-group v-model="formData.status">
-            <!-- 5 是默认值，判断是否是5  是5 传null -->
+          <!-- 5 是默认值，判断是否是5  是5 传null -->
           <el-radio label="5">全部</el-radio>
           <el-radio label="0">草稿</el-radio>
           <el-radio label="1">待审核</el-radio>
@@ -25,9 +25,7 @@
       </el-col>
       <el-col :span="18">
         <el-select placeholder="请选择" v-model="formData.channel_id">
-          <el-option v-for="item in channels" :key="item.id"
-             :label="item.name"
-             :value="item.id"></el-option>
+          <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
         </el-select>
       </el-col>
     </el-row>
@@ -36,9 +34,47 @@
         <span>时间选择：</span>
       </el-col>
       <el-col :span="18">
-        <el-date-picker v-model="formData.dateRange" type="daterange" range-separator= " 至 " start-placeholder="开始日期" end-placeholder="结束日期">
-
-        </el-date-picker>
+        <el-date-picker
+          v-model="formData.dateRange"
+          type="daterange"
+          range-separator=" 至 "
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
+      </el-col>
+    </el-row>
+    <el-row class="total">
+      <span>共找到1999条符合条件的内容</span>
+    </el-row>
+    <!-- 循环的模板 -->
+    <el-row
+      v-for="item in 100"
+      :key="item"
+      class="article-item"
+      type="flex"
+      justify="space-between"
+    >
+      <!-- 左侧 -->
+      <el-col :span="14">
+        <el-row type="flex">
+          <img src="../../assets/img/404.png" alt />
+          <div class="info">
+            <span>年少不听李宗盛，听懂己是不惑年。</span>
+            <el-tag class="tag">标签一</el-tag>
+            <span class="date">2019-12-24 09:15:42</span>
+          </div>
+        </el-row>
+      </el-col>
+      <!-- 右侧 -->
+      <el-col :span="6">
+        <el-row class="right" type="flex" justify="end">
+          <span>
+            <i class="el-icon-edit"></i>修改
+          </span>
+          <span>
+            <i class="el-icon-delete"></i> 删除
+          </span>
+        </el-row>
       </el-col>
     </el-row>
   </el-card>
@@ -78,4 +114,40 @@ export default {
   margin-left: 40px;
   margin-top: 30px;
 }
+.total {
+      margin:60px 0;
+      height: 30px;
+      border-bottom: 1px dashed #ccc;
+  }
+  .article-item {
+      margin: 20px 0;
+      padding: 10px 0;
+      border-bottom: 1px solid #f2f3f5;
+      img {
+          width: 180px;
+          height: 100px;
+          margin-right: 10px;
+          border-radius: 4px;
+      }
+      .info {
+          height: 100px;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          .tag {
+              max-width:80px;
+          }
+          .date {
+              color: #999;
+              font-size:12px;
+          }
+      }
+      .right {
+          span {
+              margin-left:8px;
+              font-size: 14px;
+              cursor: pointer;
+          }
+      }
+  }
 </style>

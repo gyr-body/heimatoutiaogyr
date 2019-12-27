2a5aac9d9c89302f1ad78e3c4e61dc144c567a08
gyr <guoyongyong-6@123.com>
2019-12-28 02:57:21 +0800

    账户信息-数据加载给表单

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -3,21 +3,22 @@
       <bread-crumb slot='header'>
          <template slot='title'>账户信息</template>
       </bread-crumb>
-      <el-upload class="head-upload" action="" :show-file-list="false">
-          <img src="../../assets/img/avatar.jpg" alt="" srcset="">
+      <!-- 上传文件组件 -->
+      <el-upload  class="head-upload" action="" :show-file-list="false">
+          <img :src="formData.photo ? formData.photo : defaultImg" alt="">
       </el-upload>
       <!-- 表单组件 -->
       <el-form style="magin-left:100px" label-width="100px">
-          <el-form-item label="用户名">
+          <el-form-item v-model="formData.name" label="用户名">
               <el-input style="width:30%"></el-input>
           </el-form-item>
-          <el-form-item label="简介">
+          <el-form-item v-model="formData.intro" label="简介">
               <el-input style="width:30%"></el-input>
           </el-form-item>
-          <el-form-item label="邮箱">
+          <el-form-item v-model="formData.email" label="邮箱">
               <el-input style="width:30%"></el-input>
           </el-form-item>
-          <el-form-item label="手机号">
+          <el-form-item v-model="formData.mobile" label="手机号">
               <el-input style="width:30%"></el-input>
           </el-form-item>
           <el-form-item>
@@ -29,7 +30,31 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      formData: {
+        name: '', // 用户名
+        intro: '', // 简介
+        photo: '', // 头像
+        email: '', // 头像
+        mobile: '' // 手机号
+      },
+      defaultImg: require('../../assets/img/admire.png')
+    }
+  },
+  methods: {
+    //   获取用户信息
+    getUserInfo () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(result => {
+        this.formData = result.data
+      })
+    }
+  },
+  created () {
+    this.getUserInfo()
+  }
 }
 </script>
 

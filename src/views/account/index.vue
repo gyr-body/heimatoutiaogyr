57d506e1c7b343449c4c9f5db273887c82f73e46
gyr <guoyongyong-6@123.com>
2019-12-28 15:38:59 +0800

    账户信息-用户数据保存

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -1,30 +1,30 @@
 <template>
-   <el-card>
-      <bread-crumb slot='header'>
-         <template slot='title'>账户信息</template>
-      </bread-crumb>
-      <!-- 上传文件组件 -->
-      <el-upload  class="head-upload" action="" :show-file-list="false">
-          <img :src="formData.photo ? formData.photo : defaultImg" alt="">
-      </el-upload>
-      <!-- 表单组件 -->
-      <el-form style="magin-left:100px" label-width="100px">
-          <el-form-item v-model="formData.name" label="用户名">
-              <el-input style="width:30%"></el-input>
-          </el-form-item>
-          <el-form-item v-model="formData.intro" label="简介">
-              <el-input style="width:30%"></el-input>
-          </el-form-item>
-          <el-form-item v-model="formData.email" label="邮箱">
-              <el-input style="width:30%"></el-input>
-          </el-form-item>
-          <el-form-item v-model="formData.mobile" label="手机号">
-              <el-input style="width:30%"></el-input>
-          </el-form-item>
-          <el-form-item>
-              <el-button type="primary"> 保存信息</el-button>
-          </el-form-item>
-      </el-form>
+  <el-card>
+    <bread-crumb slot="header">
+      <template slot="title">账户信息</template>
+    </bread-crumb>
+    <!-- 上传文件组件 -->
+    <el-upload class="head-upload" action :show-file-list="false">
+      <img :src="formData.photo ? formData.photo : defaultImg" alt />
+    </el-upload>
+    <!-- 表单组件 -->
+    <el-form ref="myForm" :model="formData" :rules="rules" style="margin-left:100px" label-width="100px">
+      <el-form-item label="用户名" prop="name">
+         <el-input v-model="formData.name" style="width:30%"></el-input>
+      </el-form-item>
+      <el-form-item label="简介" prop="intro">
+        <el-input v-model="formData.intro" style="width:30%"></el-input>
+      </el-form-item>
+      <el-form-item label="邮箱" prop="email">
+         <el-input v-model="formData.email" style="width:30%"></el-input>
+      </el-form-item>
+      <el-form-item label="手机号" prop="mobile">
+        <el-input v-model="formData.mobile" disabled style="width:30%"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="saveUserInfo" type="primary">保存信息</el-button>
+      </el-form-item>
+    </el-form>
   </el-card>
 </template>
 
@@ -39,6 +39,12 @@ export default {
         email: '', // 头像
         mobile: '' // 手机号
       },
+      // 表单校验 规则
+      rules: {
+
+        name: [{ required: true, message: '用户名不能为空' }, { min: 1, max: 7, message: '用户名的长度限制为1到7个字符' }],
+        email: [{ required: true, message: '邮箱不能为空' }, { parrern: /^([a-zA-Z]|[0-9])(\w|-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/, message: '邮箱格式不正确' }]
+      },
       defaultImg: require('../../assets/img/admire.png')
     }
   },
@@ -50,6 +56,25 @@ export default {
       }).then(result => {
         this.formData = result.data
       })
+    },
+    saveUserInfo () {
+      // 要去校验表单数据是否OK
+      this.$refs.myForm.validate((isOK) => {
+        //   调用保存方法
+        if (isOK) {
+          this.$axios({
+            url: '/user/profile',
+            method: 'patch',
+            data: this.formData
+          }).then(result => {
+            //   认为保存成功
+            this.$message({
+              type: 'success',
+              message: '保存信息成功'
+            })
+          })
+        }
+      })
     }
   },
   created () {
@@ -59,13 +84,13 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.head-upload{
-    position: absolute;
-    right: 300px;
-    img{
-        width: 200px;
-        height: 200px;
-        border-radius: 50%;
-    }
+.head-upload {
+  position: absolute;
+  right: 300px;
+  img {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+  }
 }
 </style>

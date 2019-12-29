3652e4aba1a7798be90759c5379e4874577d1398
gyr <guoyongyong-6@123.com>
2019-12-29 16:15:36 +0800

    echarts在项目中的应用

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,18 +1,90 @@
 <template>
 <!-- 二级路由组件 -->
-  <div></div>
+  <div>
+    <!-- 图表 盒子 -->
+    <div ref="myChart" class="echarts"></div>
+  </div>
 </template>
 
 <script>
+import echarts from 'echarts'
 export default {
-
+  // 需要在渲染完成事件才可以获取到dom元素
+  mounted () {
+    let myChart = echarts.init(this.$refs.myChart) // 获取实例对象
+    myChart.setOption({
+      title: {
+        text: '折线图堆叠'
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {}
+        }
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: '邮件营销',
+          type: 'line',
+          stack: '总量',
+          data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+          name: '联盟广告',
+          type: 'line',
+          stack: '总量',
+          data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+          name: '视频广告',
+          type: 'line',
+          stack: '总量',
+          data: [150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+          name: '直接访问',
+          type: 'line',
+          stack: '总量',
+          data: [320, 332, 301, 334, 390, 330, 320]
+        },
+        {
+          name: '搜索引擎',
+          type: 'line',
+          stack: '总量',
+          data: [820, 932, 901, 934, 1290, 1330, 1320]
+        }
+      ]
+    }) // 设置图表属性
+  }
 }
 </script>
 
 <style lang="less" scoped>
 div{
-    background-image: url('../../assets/img/home.jpg');
     height: 100vh;
-    background-size: cover
+    background-size: cover;
+    .echarts{
+      height: 600px;
+      width: 100vh;
+    }
 }
 </style>
